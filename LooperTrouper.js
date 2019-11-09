21b47737c0788c9bb45aeb032aae1ac598344446
Per Baltzar <per.baltzar@gmail.com>
2019-11-09 10:00:15 +0100

    plaPause function works!

--- LooperTrouper.js
+++ LooperTrouper.js
@@ -1,28 +1,43 @@
 import * as PIXI from 'pixi.js';
-import createAudioBuffer from './functions/getAudioBuffer';
+import createAudioBuffer from './functions/createAudioBuffer';
+import getBPM from './functions/getBPM';
+
+const PLAYING = 'playing';
+const PAUSED = 'paused';
 
 export default class LooperTrouper {
-  /** @property audioContext */
-  /** @property source  */
-  /** @property name  */
-  /** @property duration  */
-  /** @property size  */
-  /** @property buffer  */
-  /** @property peaks  */
+  /** @property {AudioContext} audioContext */
+  /** @property {AudioBufferSourceNode} source audio source  */
+  /** @property {AudioBuffef} buffer the audio data */
+  /** @property {String} name of audio file */
+  /** @property {Number} duration of audio file in seconds */
+  /** @property {Number} size of audio file */
+  /** @property {[Number]} peaks  the peaks from -1 to 1 */
+  /** @property {String} state current state  */
+  /** @property {Number} startTime dynamic start time from context */
+  /** @property {Number} progressTime seconds played */
+  /** @property {Number} progressPercentage percentage played */
 
   constructor(view, width, height) {
-    this.pixi = new PIXI.Application({
+    this.pixi = this.createPixi(view, width, height);
+    this.audioContext = new window.AudioContext();
+    this.state = PAUSED;
+    this.startTime = this.audioContext.currentTime;
+  }
+
+  createPixi(view, width, height) {
+    return (this.pixi = new PIXI.Application({
       view: view,
       width: width,
       height: height,
       transparent: true
-    });
-
-    this.audioContext = new window.AudioContext();
+    }));
   }
 
-  loadAudio() {
-    this.audioContext.load(audio);
+  async loadAudio(url) {
+    this.buffer = await this.getAudioBuffer(url);
+    this.bpm = await getBPM(url);
+    this.createSource();
   }
 
   saveFileInformation(file) {
@@ -30,30 +45,60 @@ export default class LooperTrouper {
     this.size = file.size;
   }
 
-  createSource(position) {
-    position = position || 0;
-    this.source = this.context.createBufferSource();
+  createSource() {
+    this.source = this.audioContext.createBufferSource();
     this.source.buffer = this.buffer;
-    this.source.connect(this.context.destination);
-    this.source.start(0, position);
+    this.source.connect(this.audioContext.destination);
   }
 
   disconnectSource() {
     this.source.disconnect();
   }
 
-  createAudioBuffer() {
-  const response = await fetch(url);
-  const buffer = await response.arrayBuffer();
-  await context.decodeAudioData(
-    buffer,
-    decoded => {
-      this.buffer = decoded;
-    },
-    error => {
-      console.log(error);
-    },
-  );
-};
+  getAudioBuffer(url) {
+    return createAudioBuffer(this.audioContext, url);
+  }
 
+  isPlaying() {
+    return this.state === PLAYING;
+  }
 
+  playPause() {
+    if (this.isPlaying()) {
+      this.setProgressTime();
+      this.pause();
+      return;
+    }
+    this.setStartTime(this.progressTime);
+    this.play(this.progressTime);
+    return;
+  }
+
+  play(position) {
+    position = position || 0;
+    this.createSource();
+    this.source.start(0, position);
+    this.state = PLAYING;
+  }
+
+  pause() {
+    this.disconnectSource();
+    this.state = PAUSED;
+  }
+
+  setStartTime(offset) {
+    offset = offset || 0;
+    this.startTime = this.audioContext.currentTime - offset;
+  }
+
+  setProgressTime() {
+    this.progressTime = this.audioContext.currentTime - this.startTime;
+  }
+
+  getProgressTime() {
+    return this.duration;
+  }
+  getProgress() {
+    return;
+  }
+}
--- index.js
+++ index.js
@@ -1,16 +1,68 @@
+import 'regenerator-runtime/runtime';
 import * as PIXI from 'pixi.js';
 import getBPM from './getBPM/index.js';
-import dropzone from './dropzone.js';
 import LooperTrouper from './LooperTrouper.js';
+import loadFile from './functions/loadFile.js';
+import validateFile from './functions/validateFile.js';
+import abba from './abba.mp3';
 
-console.log(dropzone);
+const originalPlayPauseButton = document.querySelector('.original-play-pause');
+const dropzone = document.querySelector('.drop-zone');
+const dropMessage = document.querySelector('.drop-message');
+let dragCounter = 0;
+
+dropzone.addEventListener(
+  'drop',
+  e => {
+    e.preventDefault();
+    e.stopPropagation();
+    const file = e.dataTransfer.files;
+    if (validateFile(file)) {
+      dragCounter = 0;
+      dropzone.classList.toggle('hidden');
+      dropMessage.classList.add('hidden');
+      originalTrouper.loadAudio(abba);
+      originalTrouper.saveFileInformation(file[0]);
+    }
+  },
+  false
+);
+
+dropzone.addEventListener('dragenter', e => {
+  e.preventDefault();
+  e.stopPropagation();
+  dragCounter++;
+  dropzone.classList.toggle('highlight');
+  dropMessage.classList.toggle('hidden');
+});
+
+dropzone.addEventListener('dragleave', e => {
+  e.preventDefault();
+  e.stopPropagation();
+  dragCounter--;
+  if (dragCounter < 1) {
+    dropzone.classList.toggle('highlight');
+    dropMessage.classList.toggle('hidden');
+  }
+});
+
+dropzone.addEventListener('dragover', e => {
+  e.preventDefault();
+  e.stopPropagation();
+});
+
 const originalCanvas = document.querySelector('#original');
-const loopCanvas = document.querySelector('#loop');
 
-const OriginalTrouper = new LooperTrouper(
+const originalTrouper = new LooperTrouper(
   originalCanvas,
   originalCanvas.width,
   originalCanvas.height
 );
 
-console.log(OriginalTrouper);
+console.log(originalTrouper);
+
+originalPlayPauseButton.addEventListener('click', e => {
+  console.log('playPause');
+  originalTrouper.playPause();
+});
+console.log(originalPlayPauseButton);
