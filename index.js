5201069cf4911a99b0cfcbc92ef6667b9ac01d54
Per Baltzar <per.baltzar@gmail.com>
2019-11-10 20:56:32 +0100

    controll buttons

--- index.js
+++ index.js
@@ -9,8 +9,12 @@ import abba from './abba.mp3';
 const originalPlayPauseButton = document.querySelector('.original-play-pause');
 const originalForward = document.querySelector('.original-forward');
 const originalBack = document.querySelector('.original-back');
+const loopPlayPauseButton = document.querySelector('.loop-play-pause');
+const loopForward = document.querySelector('.loop-forward');
+const loopBack = document.querySelector('.loop-back');
 const dropzone = document.querySelector('.drop-zone');
 const dropMessage = document.querySelector('.drop-message');
+const createLoop = document.querySelector('.create-loop');
 let dragCounter = 0;
 
 dropzone.addEventListener(
@@ -24,7 +28,7 @@ dropzone.addEventListener(
       dropzone.classList.toggle('hidden');
       dropMessage.classList.add('hidden');
       originalTrouper.loadAudio(abba);
-      originalTrouper.saveFileInformation(file[0]);
+      originalTrouper.setFileInformation(file[0].name, file[0].size);
     }
   },
   false
@@ -54,13 +58,21 @@ dropzone.addEventListener('dragover', e => {
 });
 
 const originalCanvas = document.querySelector('#original');
+const loopCanvas = document.querySelector('#loop');
 
 const originalTrouper = new LooperTrouper(
   originalCanvas,
   originalCanvas.width,
   originalCanvas.height
 );
+const loopTrouper = new LooperTrouper(
+  loopCanvas,
+  loopCanvas.width,
+  loopCanvas.height,
+  true
+);
 
+//==============================================================
 originalPlayPauseButton.addEventListener('click', e => {
   originalTrouper.playPause();
 });
@@ -71,4 +83,26 @@ originalForward.addEventListener('click', e => {
 
 originalBack.addEventListener('click', e => {
   originalTrouper.backFive();
+});
+
+//==============================================================
+loopPlayPauseButton.addEventListener('click', e => {
+  loopTrouper.playPause();
+  console.log(loopTrouper);
+});
+
+loopForward.addEventListener('click', e => {
+  loopTrouper.forwardFive();
+});
+
+loopBack.addEventListener('click', e => {
+  loopTrouper.backFive();
+});
+//==============================================================
+
+createLoop.addEventListener('click', e => {
+  const buffer = originalTrouper.exportLoop(5, 10);
+  originalTrouper.pause();
+  loopTrouper.setAudioBuffer(buffer);
+  loopTrouper.loadBuffer(buffer);
 });
