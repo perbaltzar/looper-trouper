e9de268db17306cf2b2f4008f8b066ef526d2a1e
Per Baltzar <per.baltzar@gmail.com>
2019-11-10 20:58:04 +0100

    copied for functions into class

--- LooperTrouper.js
+++ LooperTrouper.js
@@ -16,6 +16,7 @@ export default class LooperTrouper {
   /** @property {PIXI.Application} pixi */
   /** @property {AudioBufferSourceNode} source audio source  */
   /** @property {AudioBuffef} buffer the audio data */
+  /** @property {Number} sampleRate sample rate of the audio */
   /** @property {String} name of audio file */
   /** @property {Number} duration of audio file in seconds */
   /** @property {Number} size of audio file */
@@ -24,8 +25,11 @@ export default class LooperTrouper {
   /** @property {Number} startTime dynamic start time from context */
   /** @property {Number} progressTime seconds played */
   /** @property {Number} progressPercent percentage played */
+  /** @property {Number} firstBeat Index of first beat of suggested loop */
+  /** @property {Number} lastBeat Index of last beat of suggested loop */
+  /** @property {Boolean} looped if this is a loop */
 
-  constructor(view, width, height) {
+  constructor(view, width, height, looped) {
     this.pixi = this.createPixi(view, width, height);
     this.audioContext = new window.AudioContext();
     this.state = PAUSED;
@@ -33,8 +37,15 @@ export default class LooperTrouper {
     this.width = width;
     this.height = height;
     this.progressLocator = new ProgressLocator(0, height, this.pixi.stage);
+    this.looped = looped || false;
   }
 
+  /**
+   * return a PIXI.Application
+   * @param view HTML element to use as canvas
+   * @param width width of the element
+   * @param height height of the element
+   */
   createPixi(view, width, height) {
     return (this.pixi = new PIXI.Application({
       view: view,
@@ -45,40 +56,82 @@ export default class LooperTrouper {
     }));
   }
 
+  /**
+   * load audio buffer and get all data we need
+   * @param url Url to audio file
+   */
   async loadAudio(url) {
-    this.buffer = await this.getAudioBuffer(url);
-    this.duration = this.buffer.duration;
+    this.buffer = await createAudioBuffer(this.audioContext, url);
     this.bpm = await getBPM(url);
     this.peaks = getPeaks(this.buffer, 300);
+    this.duration = this.buffer.duration;
+    this.sampleRate = this.buffer.sampleRate;
     this.createSource();
     this.bars = this.createBars();
     this.createUpdateWaveform();
-    // this.startAnimation();
   }
 
-  saveFileInformation(file) {
-    this.name = file.name;
-    this.size = file.size;
+  loadBuffer(buffer) {
+    this.peaks = getPeaks(buffer, 300);
+    this.duration = this.buffer.duration;
+    this.sampleRate = this.buffer.sampleRate;
+    this.createSource();
+    this.bars = this.createBars();
+    this.createUpdateWaveform();
   }
 
+  /**
+   * saves file information
+   * @param name name of the file
+   * @param size size of the file
+   */
+  setFileInformation(name, size) {
+    this.name = name;
+    this.size = size;
+  }
+
+  /**
+   * creates and save a new BufferSource
+   */
   createSource() {
     this.source = this.audioContext.createBufferSource();
     this.source.buffer = this.buffer;
+    this.source.loop = this.looped;
     this.source.connect(this.audioContext.destination);
   }
 
+  /**
+   * remove current BufferSource
+   */
   disconnectSource() {
     if (this.source) this.source.disconnect();
   }
 
-  getAudioBuffer(url) {
-    return createAudioBuffer(this.audioContext, url);
+  /**
+   * return audio buffer
+   */
+  getAudioBuffer() {
+    return this.audioBuffer;
   }
 
+  /**
+   * Set audio buffer in class
+   * @param buffer Audio Buffer
+   */
+  setAudioBuffer(buffer) {
+    this.buffer = buffer;
+  }
+
+  /**
+   * returns true if player is playing
+   */
   isPlaying() {
     return this.state === PLAYING;
   }
 
+  /**
+   * pause or play the audio
+   */
   playPause() {
     if (this.isPlaying()) {
       this.setProgressTime();
@@ -90,6 +143,10 @@ export default class LooperTrouper {
     return;
   }
 
+  /**
+   * plays the audio
+   * @param position position to play from
+   */
   play(position) {
     position = position || 0;
     this.disconnectSource();
@@ -98,21 +155,30 @@ export default class LooperTrouper {
     this.state = PLAYING;
   }
 
+  /**
+   * pause the audio
+   */
   pause() {
     this.disconnectSource();
     this.state = PAUSED;
   }
 
+  /**
+   * Skips 5 seconds forward
+   */
   forwardFive() {
     this.disconnectSource();
     this.createSource();
     if (this.getProgressTime() + 5 > this.duration) {
-      // COde to set finished.
+      // Code to set finished.
     }
     this.source.start(0, this.getProgressTime() + 5);
     this.setStartTime(this.getProgressTime() + 5);
   }
 
+  /**
+   * rewinds 5 seconds
+   */
   backFive() {
     let time = this.getProgressTime() - 5;
     this.disconnectSource();
@@ -124,27 +190,46 @@ export default class LooperTrouper {
     this.setStartTime(time);
   }
 
+  /**
+   * sets a new start time
+   * @param offset if audio clip not played from beginning
+   */
   setStartTime(offset) {
     offset = offset || 0;
     this.startTime = this.audioContext.currentTime - offset;
   }
 
+  /**
+   * time in seconds of the audio clip played
+   */
   setProgressTime() {
     this.progressTime = this.audioContext.currentTime - this.startTime;
   }
 
+  /**
+   * return time in seconds of the audio clip played
+   */
   getProgressTime() {
     return this.audioContext.currentTime - this.startTime;
   }
 
+  /**
+   * saves the progress in percent
+   */
   setProgressPercent() {
     this.progressPercent = this.getProgressTime() / this.duration;
   }
 
+  /**
+   * return the progress in percent
+   */
   getProgressPercent() {
     return this.getProgressTime() / this.duration;
   }
 
+  /**
+   * return Bars based on peaks and width
+   */
   createBars() {
     let x = 0;
     const width = this.width / this.peaks.length / 2;
@@ -159,6 +244,9 @@ export default class LooperTrouper {
     return bars;
   }
 
+  /**
+   * Loop to change colors of played bars
+   */
   createUpdateWaveform() {
     this.pixi.ticker.add(() => {
       if (this.state === PLAYING) {
@@ -175,7 +263,94 @@ export default class LooperTrouper {
             this.bars[i].draw();
           }
         }
+        // Look for eventsFired when clip finished
+        if (this.getProgressPercent() > 0.99999) {
+          this.setStartTime();
+        }
       }
     });
+  }
+
+  /**
+   * returns a part of the current audio file
+   * @param start loop start in seconds
+   * @param end loop end in seconds
+   */
+  exportLoop(start, end) {
+    if (!start || !end) {
+      console.log('no duration given');
+      return {};
+    }
+    // get duration, sampleRate, start and  from region, sampleRate translate seconds to index
+    const sampleRate = this.sampleRate;
+
+    const loopDuration = (end - start) * this.sampleRate;
+
+    // Translate time to index
+    start = start * this.sampleRate;
+    end = end * this.sampleRate;
+
+    //  create an empty buffer
+    const copy = this.audioContext.createBuffer(
+      this.buffer.numberOfChannels,
+      loopDuration,
+      this.sampleRate
+    );
+
+    for (let i = 0; i < this.buffer.numberOfChannels; i++) {
+      const chanData = this.buffer.getChannelData(i);
+      let copyData = copy.getChannelData(i);
+
+      let midData = chanData.subarray(start, end);
+
+      // Make sure they're the same length
+      if (midData.length > copyData.length) {
+        midData = midData.slice(0, copyData.length);
+      } else if (midData.length < copyData.length) {
+        copyData = copyData.slice(0, midData.length);
+      }
+
+      copyData.set(midData);
+    }
+    return copy;
+  }
+
+  /**
+   * returns the time of the first real beat
+   * @param buffer Web Audio Audio Buffer
+   */
+  findFirstBeat() {
+    // calculating the resolution needed by duration 100 per second
+    const partsPerSecond = 50;
+    const resolution = Math.round(this.duration * partsPerSecond);
+
+    // Loop through and check difference between peaks, lower the need to check different songs
+    let topPeak = 1;
+
+    // lowest criteria in difference to trust the value
+    const j = 0.2;
+    let firstIndex = 0;
+    while (j < topPeak && firstIndex === 0) {
+      for (let i = 0; i < this.peaks.length; i++) {
+        const diff = this.peaks[i] - this.peaks[i + 1];
+        if (diff > topPeak && i < resolution * 0.3) {
+          firstIndex = i;
+          break;
+        }
+      }
+      topPeak -= 0.1;
+    }
+
+    // get time from index
+    this.firstBeat = (firstIndex / resolution) * this.duration;
+  }
+
+  findLastBeat(firstBeat, minDuration) {
+    const secondsPerBeat = 60 / this.bpm;
+    let lastBeat = 0;
+    for (let i = 1; lastBeat - firstBeat < minDuration; i++) {
+      lastBeat = firstBeat + secondsPerBeat * 16 * i;
+    }
+    this.lastBeat = lastBeat;
   }
 }
