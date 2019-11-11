8a5beb49a3b6598f72c700d049038d314f8d93c3
Per Baltzar <per.baltzar@gmail.com>
2019-11-11 10:20:40 +0100

    added pixi sprite to locator

--- src/ProgressLocator.js
+++ src/ProgressLocator.js
@@ -1,21 +1,9 @@
-class ProgressLocator extends PIXI.Graphics {
-  constructor(x, height, parent) {
-    super();
+class ProgressLocator extends PIXI.Sprite {
+  constructor(x, height) {
+    super(Texture.fromImage('./assets/locator.png'));
     this.x = x;
+    this.y = 0;
     this.height = height;
-    this.color = 0xffffff;
-    this.parent = parent;
-    this.lineWidth = 0.5;
-    this.lineStyle(this.lineWidth, this.color);
-    this.moveTo(x, 0);
-    this.lineTo(x, height);
-    parent.addChild(this);
-  }
-  update(x) {
-    this.clear();
-    this.lineStyle(this.lineWidth, this.color);
-    this.moveTo(x, 0);
-    this.lineTo(x, 150);
   }
 }
 
