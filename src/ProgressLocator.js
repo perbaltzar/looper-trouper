c4e78fc963ecc3fa51c7d1ef25b8ca2af8c4e759
Per Baltzar <per.baltzar@gmail.com>
2019-11-11 11:09:58 +0100

    locator works

--- src/ProgressLocator.js
+++ src/ProgressLocator.js
@@ -1,9 +1,18 @@
+import locator from './assets/locator.png';
 class ProgressLocator extends PIXI.Sprite {
-  constructor(x, height) {
-    super(Texture.fromImage('./assets/locator.png'));
+  constructor(x, height, parent) {
+    super(PIXI.Texture.from(locator));
     this.x = x;
     this.y = 0;
     this.height = height;
+    this.width = (height / 200) * 20;
+    // this.scale.set(0.5, 0.5);
+
+    parent.addChild(this);
+  }
+  tick(progress) {
+    this.x = progress - this.width / 2;
+    this.scale.set(0.15, 0.15);
   }
 }
 
