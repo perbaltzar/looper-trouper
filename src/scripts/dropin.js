93ec923ed8547f04eacff8d05cf27244fb0c7e5e
Per Baltzar <per.baltzar@gmail.com>
2019-11-12 11:13:04 +0100

    added arrow icon and rotation logic

--- src/scripts/dropin.js
+++ src/scripts/dropin.js
@@ -1,7 +1,9 @@
 const dropIn = document.querySelector('.drop-in');
 const dropInButton = document.querySelector('.drop-in-button');
+const arrowIcon = document.querySelector('.arrow-icon');
 
 dropInButton.addEventListener('click', e => {
   e.preventDefault();
   dropIn.classList.toggle('active');
+  arrowIcon.classList.toggle('rotate');
 });
