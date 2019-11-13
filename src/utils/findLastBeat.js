24aa72ccbc903fa2a10f07dd08dd09c34b9dd6a8
Per Baltzar <per.baltzar@gmail.com>
2019-11-13 15:40:19 +0100

    removed console log

--- src/utils/findLastBeat.js
+++ src/utils/findLastBeat.js
@@ -7,7 +7,6 @@
 const findLastBeat = (audio, firstBeat, minDuration) => {
   const bpm = audio.bpm;
   const secondsPerBeat = 60 / bpm;
-  console.log(secondsPerBeat);
   let lastBeat = 0;
   for (let i = 1; lastBeat - firstBeat < minDuration; i++) {
     lastBeat = firstBeat + secondsPerBeat * 16 * i;
