726069c7b859091f95c492bbe6dd64b2dab977c6
Per Baltzar <per.baltzar@gmail.com>
2019-11-15 12:56:42 +0100

    on off buttons light diodes

--- src/index.js
+++ src/index.js
@@ -38,19 +38,29 @@ const loopLooping = document.querySelector('.loop-looping');
 // Drop in buttons
 const createSmartLoop = document.querySelector('.smart-loop-button');
 const resetSong = document.querySelector('.reset-song');
+const lowPassSwitch = document.querySelector('.low-pass-button');
+const highPassSwitch = document.querySelector('.high-pass-button');
+const eqSwitch = document.querySelector('.eq-button');
 
 //=========== DIODES =================//
 const diodes = document.querySelectorAll('.diode');
 
+// original diodes
 const orgPlayDiode = document.querySelector('.org-play-diode');
 const orgLoopingDiode = document.querySelector('.original-looping-diode');
 const orgForwardDiode = document.querySelector('.original-forward-diode');
 const orgBackwardDiode = document.querySelector('.original-backward-diode');
 
+// loop diodes
 const loopPlayDiode = document.querySelector('.loop-play-diode');
 const loopForwardDiode = document.querySelector('.loop-forward-diode');
 const loopBackwardDiode = document.querySelector('.loop-backward-diode');
 const loopLoopingDiode = document.querySelector('.loop-looping-diode');
+
+// drop-in diodes
+const lowPassDiode = document.querySelector('.low-pass-diode');
+const highPassDiode = document.querySelector('.high-pass-diode');
+const eqDiode = document.querySelector('.eq-diode');
 
 //=========== DROP IN =================
 const dropIn = document.querySelector('.drop-in');
@@ -58,6 +68,13 @@ const dropIn = document.querySelector('.drop-in');
 //=========== SLIDERS =================//
 const lengthSlider = document.querySelector('#length-slider');
 const lengthDisplay = document.querySelector('.minimun-length-display');
+
+// eq
+const low = document.querySelector('#low');
+const lowMid = document.querySelector('#low-mid');
+const mid = document.querySelector('#mid');
+const highMid = document.querySelector('#high-mid');
+const high = document.querySelector('#high');
 
 //=========== DROP ZONE ===============//
 const dropzone = document.querySelector('.drop-zone');
@@ -263,4 +280,18 @@ resetSong.addEventListener('click', e => {
   dropMessageIntro.classList.remove('hidden');
   resetInformation();
   turnOffDiodes(diodes);
+});
+
+//======================== EQ =========================\\
+eqSwitch.addEventListener('click', e => {
+  eqDiode.classList.toggle('glowing');
+});
+
+//======================== LOW PASS =========================\\
+lowPassSwitch.addEventListener('click', e => {
+  lowPassDiode.classList.toggle('glowing');
+});
+//======================== High PASS =========================\\
+highPassSwitch.addEventListener('click', e => {
+  highPassDiode.classList.toggle('glowing');
 });
