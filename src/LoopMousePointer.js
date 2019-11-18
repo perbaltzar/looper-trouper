05f29507e70fdba286b970f8169ed7623c292065
Per Baltzar <per.baltzar@gmail.com>
2019-11-18 16:09:00 +0100

    Loop Mouse Pointer Added

---
+++ src/LoopMousePointer.js
@@ -0,0 +1,21 @@
+import loop from './assets/icons/loop.svg';
+
+class LoopMousePointer extends PIXI.Sprite {
+  constructor(parent) {
+    super(PIXI.Texture.from(loop));
+    this.x = 100;
+    this.y = 100;
+    this.width = 6;
+    this.height = 12;
+    this.isVisible = true;
+    this.zIndex = 999999;
+    parent.addChild(this);
+  }
+
+  tick() {
+    this.x = this.x;
+    this.y = this.y;
+  }
+}
+
+export default LoopMousePointer;
